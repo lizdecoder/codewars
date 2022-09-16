2f798869f2475ed9ae382d215d9120957d7e21ff
Liz Skert <eys.1908@gmail.com>
2022-09-16 19:00:19 -0400

    uploading reduce_but_grow.py

---
+++ reduce_but_grow.py
@@ -0,0 +1,16 @@
+# Given a non-empty array of integers, return the result of multiplying the values together in order. Example: [1, 2, 3, 4] => 1 * 2 * 3 * 4 = 24
+
+arr = [1, 2, 3, 4] # 1 * 2 * 3 * 4 = 24
+# arr = [4, 1, 1, 1, 4] #should be 16
+# arr = [2, 2, 2, 2, 2, 2]  #should be 64
+
+
+def grow(arr):
+	total = 1
+	for num in arr:
+		total *= num
+	print(f"in for {total}")
+	return total
+
+num = grow(arr)
+print(num)
