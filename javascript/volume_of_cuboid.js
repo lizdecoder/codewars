fde46d9b6c154cc487b2e88dcd5018f1bc1245e1
Liz Skert <eys.1908@gmail.com>
2022-09-20 14:11:44 -0400

    uploading volume_of_cuboid.js

---
+++ javascript/volume_of_cuboid.js
@@ -0,0 +1,8 @@
+// Bob needs a fast way to calculate the volume of a cuboid with three values: the length, width and height of the cuboid. Write a function to help Bob with this calculation.
+
+function volume(l, w, h){
+	return l*w*h;
+}
+
+v = volume(2, 3, 6)
+console.log(v)
