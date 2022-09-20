16fe27c3a72a8ca405d3797f4ea6378f4cf3a452
Liz Skert <eys.1908@gmail.com>
2022-09-20 14:07:42 -0400

    uploading convert_to_array.js

---
+++ javascript/convert_to_array.js
@@ -0,0 +1,11 @@
+// convert a string to an array
+
+function strToArray (s){
+	return s.split(" ")
+}
+
+// str = "Robin Singh"
+str = "I love arrays they are my favorite"
+
+thing = strToArray(str)
+console.log(thing)
