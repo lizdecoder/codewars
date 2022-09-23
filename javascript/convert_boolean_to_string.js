7ece8aadb8c9be4f3db1911398bb823be8d5816f
Liz Skert <eys.1908@gmail.com>
2022-09-23 15:23:55 -0400

    uploading convert_boolean_to_string.js

---
+++ javascript/convert_boolean_to_string.js
@@ -0,0 +1,11 @@
+// Implement a function which convert the given boolean value into its string representation.
+
+// Note: Only valid inputs will be given.
+
+function booleanToString(bool){
+	return bool.toString();
+}
+
+string = booleanToString(false);
+// console.log(string)
+// console.log(typeof(string))
