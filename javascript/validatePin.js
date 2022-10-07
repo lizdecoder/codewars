5b5335d4a321ce9fd5a05a8ee0b064b7fcb72108
lizdecoder <eys.1908@gmail.com>
2022-10-07 15:34:22 -0400

    uploading validatePin.js

---
+++ javascript/validatePin.js
@@ -0,0 +1,34 @@
+// ATM machines allow 4 or 6 digit PIN codes and PIN codes cannot contain anything but exactly 4 digits or exactly 6 digits.
+
+// If the function is passed a valid PIN string, return true, else return false.
+
+// Examples (Input --> Output)
+
+// "1234"   -->  true
+// "12345"  -->  false
+// "a234"   -->  false
+
+// original attempt
+// function validatePIN (pin){
+// /^-?\d+$/ all positive and negative whole numbers
+// /^\d+$/ all positive whole numbers 
+// /^\d+\.\d+$/ all positive floats
+//     if (pin.length === 4 || pin.length === 6) {
+//         console.log("made it here")
+//         return /^\d+$/.test(pin);
+//     }
+//     return false;
+// }
+
+// refractored
+function validatePIN (pin){
+    return /^(\d{4} | \d{6})$/.test(pin)
+}
+
+// bool = validatePIN("1234"); //true
+// bool = validatePIN("12345"); //false
+bool = validatePIN("a234"); //false
+// bool = validatePIN('-1234') //false
+// bool = validatePIN("-1.234") //false
+
+console.log(bool);
