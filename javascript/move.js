3d3f83184eb968dd51d84ebf34987aca7e3a87bd
lizdecoder <eys.1908@gmail.com>
2022-10-26 16:18:23 -0400

    uploading move.js

---
+++ javascript/move.js
@@ -0,0 +1,18 @@
+// Terminal game move function
+
+// In this game, the hero moves from left to right. The player rolls the dice and moves the number of spaces indicated by the dice two times.
+
+// Create a function for the terminal game that takes the current position of the hero and the roll (1-6) and return the new position.
+
+// Example:
+
+// move(3, 6) should equal 15
+
+function move(position, roll) {
+    return position + 2 * roll
+}
+
+// pos = move(0, 4); //8
+// pos = move(3, 6); //15
+pos = move(2, 5); //12
+console.log(pos)
