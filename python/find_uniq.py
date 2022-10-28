1caab851b18ae55ea9b289ff648f6dc6a3ea03f2
lizdecoder <eys.1908@gmail.com>
2022-10-28 11:43:25 -0400

    updating find_uniq.py

--- python/find_uniq.py
+++ python/find_uniq.py
@@ -6,16 +6,37 @@
 
 # The tests contain some very huge arrays, so think about performance.
 
-def find_uniq(arr):    
-    # for i in range(len(arr)):
-    #     count = arr.count(arr[i])
-    #     if count == 1:
-    #         return arr[i]
+# do not use for loop for large arrays
+# def find_uniq(arr): 
+#     for num in arr:
+#         if arr.count(num) == 1:
+#             return num
 
-    for num in arr:
-        if arr.count(num) == 1:
-            return num
+# refractored for large arrays
+# def find_uniq(arr):    
+#     arr.sort()
+#     if arr[0] != arr[len(arr)-1]:
+#         if arr[0] == arr[len(arr)-2]:
+#             return arr[len(arr)-1]
+#         return arr[0]
+
+# refractored again...
+def find_uniq(arr):
+    arr.sort()
+    return arr[0] if arr[0] != arr[1] else arr[-1]
+
+# clever attempt
+# def find_uniq(arr):   
+#     a, b = set(arr)
+#     return a if arr.count(a) == 1 else b
+
+# clever attempt 2
+# def find_uniq(arr): 
+    # by add [0] at the end, you return the value and not an array with the value
+    # return [x for x in set(arr) if arr.count(x) == 1][0]
+
 
 # unique = find_uniq([ 1, 1, 1, 2, 1, 1 ]) # 2
-unique = find_uniq([ 0, 0, 0.55, 0, 0 ]) # 0.55
+# unique = find_uniq([ 0, 0, 0.55, 0, 0 ]) # 0.55
+unique = find_uniq([4, 4, 4, 3, 4, 4, 4]) # 3
 print(unique)
